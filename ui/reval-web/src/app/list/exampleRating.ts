275d47ff4fd3c3e6520235969554995e7cb4c07b
Tobias Trompell <tobiastrompell@aol.com>
2023-08-23 11:11:09 +0200

    First set of changes to connect the data to ui

---
+++ ui/reval-web/src/app/list/exampleRating.ts
@@ -0,0 +1,39 @@
+export var ratings = {
+    "scenarios": [
+        {
+            "id": "7d1e8d9a-f0c6-4f12-86d4-6956e1b47e01",
+            "name": "Szenario 1",
+            "desctiption": "Das ist das erste tolle Szenario mit den Antworten aus Llama-2-13b-chat-hf_responses_rated_by_gpt-3.5-turbo.json",
+            "totalResponseCount": 400,
+            "progressStatistic": [
+                {
+                    "name": "rated",
+                    "value": 70
+                },
+                {
+                    "name": "unrated",
+                    "value": 330
+                }
+            ],
+            "resultStatistic": [
+                {
+                    "name": "positive",
+                    "value": 12
+                },
+                {
+                    "name": "negative",
+                    "value": 46
+                },
+                {
+                    "name": "neutral",
+                    "value": 12
+                }
+            ],
+            "ratingScore": {
+                "min": -1,
+                "value": 0.8,
+                "max": 1
+            }
+        }
+    ]
+}
