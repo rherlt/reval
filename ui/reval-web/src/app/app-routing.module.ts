0a227cb9c24049818016b2f8d3a4d6c2d9899930
Rico Herlt <mail@rherlt.com>
2023-09-27 14:03:18 +0200

    Rename 'list' to statistics'

--- ui/reval-web/src/app/app-routing.module.ts
+++ ui/reval-web/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ListComponent } from './statistics/statistics.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 import { EvaluationComponent } from './evaluation/evaluation.component';
 
 const routes: Routes = [
   { path: '', component: EvaluationComponent },
-  { path: 'list', component: ListComponent }
+  { path: 'scenario', component: StatisticsComponent }
 ];
 
 @NgModule({
